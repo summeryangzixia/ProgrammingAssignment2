bab43d6dabf88736f088bf10aa2e765fc89ab48e
summeryangzixia <Summer@Zixias-MacBook-Pro.local>
2016-02-16 16:21:19 -0500

    Revert "coursera_ProgrammingAssignment2"
    This reverts commit 9abc1c0a9c3791120276e3ba70a8709baa819ced.

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,15 @@
-## Belowing two of my function work as caching the inverse of a matrix.
+## Put comments here that give an overall description of what your
+## functions do
 
-## The first function, makeCacheMatrix creates a special "matrix", which can cache its inverse.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-	m<-NULL
-	set<-function(y){
-		x<<-y
-		m<<-NULL
-	}
-	get<-function() x
-	setinverse<-function(inverse) m<<-inverse
-	getinverse<-function() m
-	list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+
 }
 
 
-## The second function, cacheSolve computes the inverse of the special "matrix" created by makeCacheMatrix above. If the inverse has already been calculated, then it should retrieve the inverse from the cacha.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m<-x$getinverse()
-        if(!is.null(m)){
-        	message("getting cached data")
-        	return(m)
-        }
-        data<-x$get()
-        m<-solve(data,...)
-        x$setinverse(m)
-        m
 }
